14638d65e42f5ef0b535fe7d2deabe981ca367a9
Eric Sapsford <esapsgit@gmail.com>
2023-07-14 09:00:56 -0400

    git hub just saved me a huge loss there

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -11,8 +11,74 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
 //============================== END POINTS ==============================
+//------- GET ALL GROUPES JOINED OR ORGANIZED BY THE CURRENT USER --------
+
+/* EXISTING ISSUES WITH OUTPUT
+
+- see if "private" needs to be either "true" or "false"
+- set models createdAt and updatedAt to grab that date literal thing rather than the default
+
+*/
+
+router.get("/current", requireAuth, async (req, res) => {
+  const { user } = req
+
+  let groupes = await Groupe.findAll({
+    raw: true,
+    include: [{
+      model: Membership,
+      attributes: [],
+      where: {
+        userId: user.id
+      }
+    }],
+    attributes: {
+      // ^C ^V you sweet sweet siren
+      include: [
+        [sequelize.fn("COUNT", sequelize.col("Memberships.id")), "numMembers"]
+      ]
+    },
+  })
+
+  //copypaste from get all down there
+  //------------------------------------------------------------------
+  for (let index = 0; index < groupes.length; index++) {
+    // let image = await Groupe.findall(
+    let image = await Groupe.findByPk(groupes[index].id, {
+      raw: true,
+      include: [{
+        model: GroupImage,
+        where: {
+          preview: true
+        },
+        attributes: ["url"]
+      }]
+    });
+
+    groupes[index].numMembers = Number(groupes[index].numMembers);
+
+    if (image) {
+      groupes[index].previewImage = image["GroupImages.url"];
+    } else {
+      groupes[index].previewImage = "preview does not exist";
+    };
+  }
+  //------------------------------------------------------------------
+
+  res.json({
+    //this will ALWAYS feel wrong
+    Groups: groupes
+  });
+})
+
 //--------------------------- GET ALL GROUPES ----------------------------
 
+/* EXISTING ISSUES WITH OUTPUT
+
+- see if "private" needs to be either "true" or "false"
+- set models createdAt and updatedAt to grab that date literal thing rather than the default
+
+*/
 
 router.get("/", async (req, res) => {
 
@@ -24,11 +90,12 @@ router.get("/", async (req, res) => {
     }],
     attributes: {
       include: [
+        // fuck for loops
+        // FUCK WHILE LOOPS
         // sequelize.fn("COUNT", sequelize.col("Memberships.id")), "AS" "numMembers"
         [sequelize.fn("COUNT", sequelize.col("Memberships.id")), "numMembers"]
       ]
     },
-    group: ["Groupe.id"]
   });
 
   // THEY SAY WE LAZY LOAD WE LAZY LOAD
